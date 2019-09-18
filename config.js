24807b5924c8ad58ec84760ea0db84feccaca956
iKroos <55063785+iKroos@users.noreply.github.com>
2019-09-18 16:01:39 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    TOKEN: 'NjIxOTU4NDQwOTAxOTM1MTA0.XXs59g.IjY7ouRMYHMO2uYHzFw7JRnmWRU',    
+    YT_API_KEY: 'api ', 
+    prefix: 'k',
+    devs: ['527825056860995594']
+}
---
+++ server.js
@@ -0,0 +1,89 @@
+
+
+
+vaulted-emperor
+
+Show
+server.js
+                name: "🔊 Volume",
+                value: `I set the volume to: **${args[1]}**`
+              }
+            ]
+          }
+        }).then(message =>{message.delete(5000)})
+    } else if (msg.content.startsWith(`${prefix}np`)) {
+        console.log(`${msg.author.tag} has been used the ${prefix}np command in ${msg.guild.name}`);
+        if (!serverQueue) return msg.channel.send({embed: {
+            color: 15158332,
+            fields: [{
+                name: "❌ Error",
+                value: 'There is nothing playing that I could skip for you.'
+              }
+            ]
+          }
+        }).then(message =>{message.delete(5000)})
+        return msg.channel.send({embed: {
+            color: 15158332,
+            fields: [{
+                name: "🎵 Now playing",
+                value: `**${serverQueue.songs[0].title}**`
+              }
+            ]
+          }
+        }).then(message =>{message.delete(5000)})
+    } else if (msg.content.startsWith(`${prefix}queue`)) {
+        console.log(`${msg.author.tag} has been used the ${prefix}queue command in ${msg.guild.name}`);
+        if (!serverQueue) return msg.channel.send({embed: {
+            color: 15158332,
+            fields: [{
+                name: "❌ Error",
+                value: 'There is nothing playing that I could skip for you.'
+              }
+            ]
+          }
+        }).then(message =>{message.delete(5000)})
+        return msg.channel.send({embed: {
+            color: 15158332,
+            fields: [{
+                name: "📋 Song queue",
+                value: `${serverQueue.songs.map(song => `**- ${song.title}**`).join('\n')}`
+              },
+              {
+                  name: "🎵 Now playing",
+                  value: `**${serverQueue.songs[0].title}**`
+              }
+            ]
+          }
+        }).then(message =>{message.delete(5000)})
+        } else if(msg.content.startsWith(`${prefix}1331`)) {
+        console.log(`${msg.author.tag} has been used the ${prefix}help command in ${msg.guild.name}`);
+ 
+        msg.channel.send('Please check your direct messages :inbox_tray:').then(message =>{message.delete(5000)})
+ 
+        msg.react('✅');
+ 
+        msg.author.send({embed: {
+            color: 15158332,
+            author: {
+              name: client.user.username,
+              icon_url: client.user.avatarURL
+            },
+            fields: [{
+                name: "Bot's commands:",
+                value: `**${prefix}help** - This message!\n\
+**${prefix}play** - Play a song from YouTube.\n\
+**${prefix}skip** - Skip a song.\n\
+**${prefix}stop** - Stops the music.\n\
+**${prefix}volume** - Change the volume of the bot.\n\
+**${prefix}np** - The song that now playing.\n\
+**${prefix}queue** - See the queue of songs.\n\
+**${prefix}pause** - Pause the music.\n\
+**${prefix}resume** - Resume the music.`
+              }
+            ],
+            timestamp: new Date(),
+            footer: {
+              icon_url: client.user.avatarURL,
+              text: "© Misaka"
+            }
+          }
